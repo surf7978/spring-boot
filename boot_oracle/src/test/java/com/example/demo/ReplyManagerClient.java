c7f4109119c47b3100a232baa4162f9c1aa2d651
surf7978 <surf7978@naver.com>
2021-04-07 10:09:26 +0900

    21.04.07

--- boot_oracle/src/test/java/com/example/demo/ReplyManagerClient.java
+++ boot_oracle/src/test/java/com/example/demo/ReplyManagerClient.java
@@ -5,6 +5,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 //이거 수동으로 추가함
 import static org.hamcrest.Matchers.is;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.junit.Test;
@@ -27,11 +29,20 @@ public class ReplyManagerClient {
 	@Test
 	//여기서는 자동 롤백인데 커밋해주고 싶으면 @Commit 적어주면 됨
 	@Commit
-	public void save() {
+	public void 댓글저장() { //Test에는 메소드명 한글로 해도됨
 		Reply reply = Reply.builder()
 								.title("test1")
 								.build();
 		em.persist(reply); //이게 저장임
 		assertThat(em.find(Reply.class, reply.getSeq()), is(reply));
+	}
+	
+	@Test
+	public void 댓글조회() {
+		//EntityManager에서 persist merge remove find가 CRUD임
+		//여기서 쓰는 sql은 jpql로 써야함
+		List<Reply> list = em.createQuery("from Reply", Reply.class) //대소문자 구별함
+							.getResultList();
+		System.out.println(list);
 	}
 }
