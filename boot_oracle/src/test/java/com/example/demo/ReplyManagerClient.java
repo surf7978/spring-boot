21487ba8aa57c406e922e9f73d728a7d852286c5
surf7978 <surf7978@naver.com>
2021-04-07 09:54:22 +0900

    21.04.07

--- boot_oracle/src/test/java/com/example/demo/ReplyManagerClient.java
+++ boot_oracle/src/test/java/com/example/demo/ReplyManagerClient.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+//이거 수동으로 추가함2
+import static org.hamcrest.MatcherAssert.assertThat;
+//이거 수동으로 추가함
+import static org.hamcrest.Matchers.is;
+
 import javax.persistence.EntityManager;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,10 +26,12 @@ public class ReplyManagerClient {
 	
 	@Test
 	//여기서는 자동 롤백인데 커밋해주고 싶으면 @Commit 적어주면 됨
+	@Commit
 	public void save() {
 		Reply reply = Reply.builder()
-								.title("test")
+								.title("test1")
 								.build();
 		em.persist(reply); //이게 저장임
+		assertThat(em.find(Reply.class, reply.getSeq()), is(reply));
 	}
 }
